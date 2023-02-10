3d298dac08324a4d29e0063d9c93b9535a3e9e5c
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2023-02-10 09:06:27 -0800

    Update download_run_level.py

--- utils/bids/download_run_level.py
+++ utils/bids/download_run_level.py
@@ -16,32 +16,32 @@ def download_bids_for_runlevel(
     project_finder = 'group=' + group_label + ',label=' + project_label
     project = client.projects.find_one(project_finder)
     #run_level is in ["project", "subject", "session", "acquisition"]
+    file_of_interest = 'fmriprep_'
     if run_level == 'project':
         sessions = project.sessions()
         for s in sessions:   
             sub_name = s.subject.label
             prep_list = []
             createdTime_list = []
-            if sub_name.startswith(sub_of_interest):
-                analyses = s.reload()['analyses']
-                session_name = s.label
-                for a in analyses:
-                    if a.gear_info:
-                        if (a.gear_info.name == file_of_interest and a.files):
-                            analysisID =  a._id
-                            prep_list.append(a._id)
-                            createdTime_list.append(a.created.strftime("%Y-%m-%d"))
-                if len(prep_list) > 1:
-                    latest_index = createdTime_list.index(max(createdTime_list))
-                    analysisID = prep_list[latest_index]
-                if prep_list:
-                    print('start download sub-' + sub_name + '_session-' + session_name)
-                    analysis = client.get(analysisID)
-                    for f in analysis.files:
-                        if f.name.startswith(file_of_interest):
-                            output_file_name = os.path.join(output_dir,'fmriprep/zip_files',f.name)
-                            if not os.path.exists(output_file_name):
-                                   f.download(output_file_name)
+            analyses = s.reload()['analyses']
+            session_name = s.label
+            for a in analyses:
+                if a.gear_info:
+                    if (a.gear_info.name == file_of_interest and a.files):
+                        analysisID =  a._id
+                        prep_list.append(a._id)
+                        createdTime_list.append(a.created.strftime("%Y-%m-%d"))
+            if len(prep_list) > 1:
+                latest_index = createdTime_list.index(max(createdTime_list))
+                analysisID = prep_list[latest_index]
+            if prep_list:
+                print('start download sub-' + sub_name + '_session-' + session_name)
+                analysis = client.get(analysisID)
+                for f in analysis.files:
+                    if f.name.startswith(file_of_interest):
+                        output_file_name = os.path.join(output_dir,'fmriprep/zip_files',f.name)
+                        if not os.path.exists(output_file_name):
+                               f.download(output_file_name)
     else:
         destination_id = gtk_context.destination["id"]
         destination = client.get(destination_id)
