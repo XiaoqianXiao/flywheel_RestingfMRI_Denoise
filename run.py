aac1c52d05ba041fffa0868bb88d18f1ac38bcf5
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2023-02-24 10:27:11 -0800

    Update run.py

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ from flywheel_gear_toolkit.utils.zip_tools import unzip_archive, zip_output
 
 from utils.bids.download_run_level import download_bids_for_runlevel
 from utils.bids.run_level import get_analysis_run_level_and_hierarchy
+from utils.singularity import run_in_tmp_dir
 
 log = logging.getLogger(__name__)
 GEAR = "RestingfMRI_Denoising"
