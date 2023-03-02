c9eb85836d397d5d5cacfc8878e9b2fe4d5c8c06
XiaoXiaoqian <33399278+XiaoXiaoqian@users.noreply.github.com>
2023-03-01 21:01:59 -0800

    Update run.py

--- run.py
+++ run.py
@@ -60,30 +60,31 @@ def generate_command(gtk_context, config, work_dir, output_analysis_id_dir, erro
     log_to_file = False
     config_keys = config.keys()
     if 'use_custom_pipline' in config_keys:
-        use_custom_pipline = config['use_custom_pipline']
-        pipeline_json_dict = {}
-        if use_custom_pipline is not None:
-            pipeline_json_dict['name'] = config['name']
-            pipeline_json_dict['description'] = config['description']
-            for k in ["wm", "csf", "gs", "motion"]:
-                pipeline_json_dict["confounds"] = {}
-                key_temp_deriv = k + '-temp_deriv'
-                key_quad_terms = k + '-quad_terms'
-                if config[k] == "fasle":
-                    pipeline_json_dict["confounds"][k] = "False"
+        if config['use_custom_pipline'] == True:
+            use_custom_pipline = config['use_custom_pipline']
+            pipeline_json_dict = {}
+            if use_custom_pipline is not None:
+                pipeline_json_dict['name'] = config['name']
+                pipeline_json_dict['description'] = config['description']
+                for k in ["wm", "csf", "gs", "motion"]:
+                    pipeline_json_dict["confounds"] = {}
+                    key_temp_deriv = k + '-temp_deriv'
+                    key_quad_terms = k + '-quad_terms'
+                    if config[k] == "fasle":
+                        pipeline_json_dict["confounds"][k] = "False"
+                    else:
+                        pipeline_json_dict["confounds"][k] = {}
+                        pipeline_json_dict["confounds"][k]["temp_deriv"] = config[key_temp_deriv]
+                        pipeline_json_dict["confounds"][k]["quad_terms"] = config[key_quad_terms]
+                pipeline_json_dict["confounds"]["acompcor"] = config["acompcor"]
+                pipeline_json_dict["aroma"] = config["aroma"]
+                if config["spikes"] == "false":
+                    pipeline_json_dict["spikes"] = "False"
                 else:
-                    pipeline_json_dict["confounds"][k] = {}
-                    pipeline_json_dict["confounds"][k]["temp_deriv"] = config[key_temp_deriv]
-                    pipeline_json_dict["confounds"][k]["quad_terms"] = config[key_quad_terms]
-            pipeline_json_dict["confounds"]["acompcor"] = config["acompcor"]
-            pipeline_json_dict["aroma"] = config["aroma"]
-            if config["spikes"] == "false":
-                pipeline_json_dict["spikes"] = "False"
-            else:
-                pipeline_json_dict["fd_th"] = config["fd_th"]
-                pipeline_json_dict["dvars_th"] = config["dvars_th"]
-            json_object = json.dumps(pipeline_json_dict, indent = 4)      
-            command_parameters['pipelines'] = json_object
+                    pipeline_json_dict["fd_th"] = config["fd_th"]
+                    pipeline_json_dict["dvars_th"] = config["dvars_th"]
+                json_object = json.dumps(pipeline_json_dict, indent = 4)      
+                command_parameters['pipelines'] = json_object
     # Validate the command parameter dictionary - make sure everything is
     # ready to run so errors will appear before launching the actual gear
     # code.  Add descriptions of problems to errors & warnings lists.
